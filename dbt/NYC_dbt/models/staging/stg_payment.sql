217265c84983064740eb2600b577df3a23a5c448
Nadimab <nadim.abouchrouch@viseo.com>
2023-05-22 16:17:16 +0200

    Clean: Removing unnecessary files

--- dbt/NYC_dbt/models/staging/stg_payment.sql
+++
@@ -1,4 +0,0 @@
-select *,
-CURRENT_TIMESTAMP() as LDTS,
-'STATIC REFERENCE DARA' as RSCR
-from {{ source('taxi_source', 'payment') }}
--- dbt/NYC_dbt/models/staging/stg_pointOfInterest.sql
+++
@@ -1,2 +0,0 @@
-select *
-from {{ source('taxi_source', 'point_of_interest') }}
